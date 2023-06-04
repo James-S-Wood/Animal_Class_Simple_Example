c8a2419916f7adf03380b98f1c9a1d1dc0aaba23
James Wood <james.s.wood@icloud.com>
2023-06-04 06:14:19 -0400

    Corrected syntax issues.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,51 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /*Here is a sample Java code that includes three classes, inheritance,
+        encapsulation, and polymorphism. */
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        class Animal {
+            private String name;
+            private int age;
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+            public Animal(String name, int age) {
+                this.name = name;
+                this.age = age;
+            }
+
+            public void makeSound() {
+                System.out.println("The animal makes a sound");
+            }
+        }
+
+        class Dog extends Animal {
+            public Dog(String name, int age) {
+                super(name, age);
+            }
+
+            @Override
+            public void makeSound() {
+                System.out.println("The dog barks");
+            }
+        }
+
+        class Cat extends Animal {
+            public Cat(String name, int age) {
+                super(name, age);
+            }
+
+            @Override
+            public void makeSound() {
+                System.out.println("The cat meows");
+            }
         }
     }
 }
+
+        /* In this example, we have three classes: `Animal`, `Dog`, and `Cat`.
+        The `Dog` and `Cat` classes inherit from the `Animal` class. The `Animal` class has two private fields: `name` and `age`. It also has a constructor that takes in a name and an age. The `Dog` and `Cat` classes have their own constructors that call the constructor of the parent class using the `super()` method.
+         */
+
+        /*The `Animal` class has a method called `makeSound()`.
+        This method is overridden in both the `Dog` and `Cat` classes to produce different sounds. This is an example of polymorphism.
+        */
